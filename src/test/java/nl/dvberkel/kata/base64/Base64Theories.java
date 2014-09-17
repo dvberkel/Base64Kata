ef123b938df92e7a03803ae74482cae5b59d1516
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-09-17 16:02:29 +0200

    create commutative theory

--- src/test/java/nl/dvberkel/kata/base64/Base64Theories.java
+++ src/test/java/nl/dvberkel/kata/base64/Base64Theories.java
@@ -7,6 +7,9 @@ import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -22,12 +25,29 @@ public class Base64Theories {
     }
 
     @DataPoint
-    public static byte[] input = new byte[] {0b0};
+    public static byte[] singleByte = new byte[] {0b0};
+
+    @DataPoint
+    public static byte[] tripleBytes = new byte[] {0b0, 0b0, 0b0};
 
     @Theory
     public void decodeIsTheInverseOfEncode(byte[] input) {
         assumeThat(input.length, is(greaterThan(0)));
 
         assertThat(kata.decode(kata.encode(input)), is(input));
+    }
+
+    @Theory
+    public void encodeAndConcatenationAreCommutative(byte[] a, byte[] b) throws IOException {
+        assumeThat(a.length % 3, is(0));
+        assumeThat(a.length, is(greaterThan(0)));
+        assumeThat(b.length, is(greaterThan(0)));
+
+        ByteArrayOutputStream s = new ByteArrayOutputStream();
+        s.write(a);
+        s.write(b);
+        byte[] concat = s.toByteArray();
+
+        assertThat(kata.encode(a).concat(kata.encode(b)), is(kata.encode(concat)));
     }
 }
